abab6efb3f53ce3c412d87b9ced6076f673a07ca
Rasul Kireev <me@rasulkireev.com>
2025-10-12 17:17:34 +0300

    print-before-send

--- tuxseo/sentry_utils.py
+++ tuxseo/sentry_utils.py
@@ -4,6 +4,16 @@ logger = get_tuxseo_logger(__name__)
 
 
 def before_send(event, hint):
-    logger.info("[BeforeSend] Event", event=event, hint=hint)
+    print("=" * 50)
+    print("SENTRY BEFORE_SEND CALLED")
+    print("=" * 50)
+
+    # Print event structure
+    print("\n[EVENT]")
+    print(event)
+
+    # Print hint structure
+    print("\n[HINT]")
+    print(hint)
 
     return event
